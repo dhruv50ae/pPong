939e5c30eba5380f429a2a3c98fb0ebc006348a4
dhruv50ae <linkdp2012@gmail.com>
2022-07-16 13:24:02 +0100

    Create Paddle and functional code

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.tracer(0)
 
 
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
+def goUp():
+    newY = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), newY)
+
+def goDown():
+    newY = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), newY)
+
+screen.listen()
+screen.onkey(goUp, "w")
+screen.onkey(goDown, "s")
+
+gameIsOn = True
+while gameIsOn:
+    screen.update()
 
 screen.exitonclick()
